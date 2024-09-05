746e6cbb050e265497b37cec40db9e0bea517d50
Olga Kruglova <olha.kruhlova@inetum.com>
2024-09-05 15:33:57 +0200

    #customize preview PDF toolbar

--- src/Components/PDFViewer.tsx
+++ src/Components/PDFViewer.tsx
@@ -1,67 +1,143 @@
-import { XMarkIcon } from "@heroicons/react/24/solid";
+import {
+  XMarkIcon,
+  MagnifyingGlassPlusIcon,
+  MagnifyingGlassMinusIcon,
+  ChevronUpIcon,
+  ChevronDownIcon,
+  ArrowDownTrayIcon,
+  PrinterIcon
+} from "@heroicons/react/24/solid";
 import { ProgressBar, Viewer, Worker } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
-import { defaultLayoutPlugin, ToolbarSlot } from "@react-pdf-viewer/default-layout"; // Correct import for ToolbarSlot
+import { defaultLayoutPlugin, ToolbarSlot } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { useContext, useEffect } from "react";
 import { ThemeContext } from "../providers/ThemeContext";
+import { RenderZoomOutProps, RenderZoomProps } from "@react-pdf-viewer/zoom";
+import { RenderGoToPageProps, RenderNumberOfPagesProps } from "@react-pdf-viewer/page-navigation";
 
 export const customToolbar = (Toolbar: any) => (
   <Toolbar>
     {(slots: ToolbarSlot) => {
       const {
-        NumberOfPages,
-        Zoom,
-        ZoomIn,
-        ZoomOut,
+        CurrentPageInput,
         GoToNextPage,
         GoToPreviousPage,
-        CurrentPageInput,
+        ZoomIn,
+        Zoom,
+        ZoomOut,
         Download,
-        Print
+        Print,
+        NumberOfPages
       } = slots;
 
       return (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            width: "100%",
-            padding: "0 20px"
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center"
-            }}
-          >
-            <NumberOfPages />
-            <GoToPreviousPage />
+        <div className="flex items-center justify-between bg-dark-blue w-full px-4 py-2 text-white">
+          <div className="flex items-center space-x-2">
+            <GoToPreviousPage>
+              {(props: RenderGoToPageProps) => (
+                <button
+                  style={{
+                    border: "none",
+                    cursor: props.isDisabled ? "not-allowed" : "pointer",
+                    padding: "8px"
+                  }}
+                  disabled={props.isDisabled}
+                  onClick={props.onClick}
+                >
+                  <ChevronUpIcon className={`${props.isDisabled ? "text-gray-600" : "text-light-blue"} size-6`} />
+                </button>
+              )}
+            </GoToPreviousPage>
             <CurrentPageInput />
-            <GoToNextPage />
+            <NumberOfPages>
+              {(props: RenderNumberOfPagesProps) => (
+                <div className="p-2 border-none text-light-blue">of {props.numberOfPages}</div>
+              )}
+            </NumberOfPages>
+            <GoToNextPage>
+              {(props: RenderGoToPageProps) => (
+                <button
+                  style={{
+                    border: "none",
+                    cursor: props.isDisabled ? "not-allowed" : "pointer",
+                    padding: "8px"
+                  }}
+                  disabled={props.isDisabled}
+                  onClick={props.onClick}
+                >
+                  <ChevronDownIcon className={`${props.isDisabled ? "text-gray-600" : "text-light-blue"} size-6`} />
+                </button>
+              )}
+            </GoToNextPage>
           </div>
 
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center"
-            }}
-          >
-            <ZoomOut />
-            <Zoom />
-            <ZoomIn />
-          </div>
+          <div className="flex items-center space-x-2">
+            <ZoomOut>
+              {(props: RenderZoomOutProps) => (
+                <button
+                  style={{
+                    border: "none",
+                    cursor: "pointer",
+                    padding: "8px"
+                  }}
+                  onClick={props.onClick}
+                >
+                  <MagnifyingGlassMinusIcon className="text-light-blue size-6" />
+                </button>
+              )}
+            </ZoomOut>
 
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center"
-            }}
-          >
-            <Download />
-            <Print />
+            <Zoom>
+              {(props: RenderZoomProps) => (
+                <span className="text-light-blue">{`${Math.round(props.scale * 100)}%`}</span>
+              )}
+            </Zoom>
+
+            <ZoomIn>
+              {(props: RenderZoomOutProps) => (
+                <button
+                  style={{
+                    border: "none",
+                    cursor: "pointer",
+                    padding: "8px"
+                  }}
+                  onClick={props.onClick}
+                >
+                  <MagnifyingGlassPlusIcon className="text-light-blue size-6" />
+                </button>
+              )}
+            </ZoomIn>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Download>
+              {(props: any) => (
+                <button
+                  style={{
+                    border: "none",
+                    cursor: "pointer",
+                    padding: "8px"
+                  }}
+                  onClick={props.onClick}
+                >
+                  <ArrowDownTrayIcon className="text-light-blue size-6" />
+                </button>
+              )}
+            </Download>
+            <Print>
+              {(props: any) => (
+                <button
+                  style={{
+                    border: "none",
+                    cursor: "pointer",
+                    padding: "8px"
+                  }}
+                  onClick={props.onClick}
+                >
+                  <PrinterIcon className="text-light-blue size-6" />
+                </button>
+              )}
+            </Print>
           </div>
         </div>
       );
